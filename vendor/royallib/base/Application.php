a75eba994ac780460da8f7207eefb6b3e06dcd70
fadi <fadich95@gmail.com>
2017-01-19 11:05:36 +0200

    ``` console

--- vendor/royallib/base/Application.php
+++ vendor/royallib/base/Application.php
@@ -66,7 +66,8 @@ final class Application extends Object
 
     private static function startScreen()
     {
-        echo "Hello";
+        $res = 'Hello!';
+        print_r($res);
     }
 
     private function call()
